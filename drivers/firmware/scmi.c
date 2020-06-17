c9108f73c056226a30bf3f5bbc1835aa70f67933
Etienne Carriere <etienne.carriere@linaro.org>
2020-06-17 20:53:20 +0200

    [draft] firmware: SCMI over OP-TEE
    Add OP-TEE transport for SCMI where SCMI messages are passed by
    invocation of the core service for non-secure kernel clients.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- drivers/firmware/scmi.c
+++ drivers/firmware/scmi.c
@@ -11,6 +11,7 @@
 #include <mailbox.h>
 #include <memalign.h>
 #include <scmi.h>
+#include <tee.h>
 #include <asm/system.h>
 #include <asm/types.h>
 #include <dm/device-internal.h>
@@ -341,6 +342,231 @@ static int probe_arm_smc_channel(struct udevice *dev)
 	return rc;
 }
 
+/* OP-TEE channel */
+struct scmi_optee_channel {
+	struct udevice *tee;
+	u32	tee_session;
+	u32	channel_id;
+	struct	scmi_shm_buf shm_buf;
+};
+
+#define TA_SCMI_UUID { 0xa8cfe406, 0xd4f5, 0x4a2e, \
+		      { 0x9f, 0x8d, 0xa2, 0x5d, 0xc7, 0x54, 0xc0, 0x99 } }
+
+/*
+ * API of commands supported the SCMI TA in OP-TEE
+ *
+ * TA_CMD_GET_CHANNEL - Get channel identifer for a buffer pool
+ * param[0] (in value) - Message buffer physical start address
+ * param[1] (in value) - Message buffer byte size
+ * param[2] (out value) - value.a: Output channel identifier
+ *
+ * TA_CMD_PROCESS_CHANNEL - Process message in SCMI channel
+ * param[0] (in value) - value.a: SCMI channel identifier
+ */
+#define TA_CMD_GET_CHANNEL_ID		0x1
+#define TA_CMD_PROCESS_CHANNEL		0x2
+
+static int tee_ret2errno(u32 tee_retval)
+{
+	switch (tee_retval) {
+	case TEE_SUCCESS:
+		return 0;
+	case TEE_ERROR_SHORT_BUFFER:
+		return -ETOOSMALL;
+	case TEE_ERROR_OUT_OF_MEMORY:
+		return -ENOMEM;
+	case TEE_ERROR_ITEM_NOT_FOUND:
+		return -ENOENT;
+	default:
+		return -EPROTO;
+	}
+}
+
+static int open_optee_session(struct scmi_optee_channel *chan)
+{
+	struct udevice *tee = NULL;
+	const struct tee_optee_ta_uuid uuid = TA_SCMI_UUID;
+
+	struct tee_open_session_arg arg;
+	int rc = 0;
+
+	if (chan->tee)
+		return 0;
+
+	tee = tee_find_device(tee, NULL, NULL, NULL);
+	if (!tee)
+		return -ENODEV;
+
+	memset(&arg, 0, sizeof(arg));
+	tee_optee_ta_uuid_to_octets(arg.uuid, &uuid);
+	rc = tee_open_session(tee, &arg, 0, NULL);
+	if (rc) {
+		dev_err(chan->dev, "Failed to invoke OP-TEE\n");
+	} else {
+		chan->tee = tee;
+		chan->tee_session = arg.session;
+	}
+
+	return rc;
+}
+
+static int get_optee_channel(struct scmi_optee_channel *chan)
+{
+	struct tee_invoke_arg arg;
+	struct tee_param param[3];
+
+	if (!chan || !chan->tee)
+		return -EINVAL;
+
+	memset(&arg, 0, sizeof(arg));
+	arg.func = TA_CMD_GET_CHANNEL_ID;
+	arg.session = chan->tee_session;
+
+	memset(param, 0, sizeof(param));
+	param[0].attr = TEE_PARAM_ATTR_TYPE_VALUE_INOUT;
+	param[1].attr = TEE_PARAM_ATTR_TYPE_VALUE_INPUT;
+	param[1].u.value.a = (u64)(uintptr_t)chan->shm_buf.buf >> 32;
+	param[1].u.value.b = (u32)chan->shm_buf.buf;
+	param[2].attr = TEE_PARAM_ATTR_TYPE_VALUE_INPUT;
+	param[2].u.value.a = chan->shm_buf.size;
+
+	if (tee_invoke_func(chan->tee, &arg, ARRAY_SIZE(param), param)) {
+		dev_err(chan->dev, "Failed to invoke OP-TEE\n");
+		return -EIO;
+	}
+
+	if (!arg.ret)
+		chan->channel_id = param[2].u.value.a;
+
+	return tee_ret2errno(arg.ret);
+}
+
+static int optee_process_msg(struct udevice *dev, struct scmi_msg *msg)
+{
+	struct scmi_agent *agent = dev_get_priv(dev);
+	struct scmi_optee_channel *chan = agent->method_priv;
+	struct tee_invoke_arg arg;
+	struct tee_param param[1];
+	struct scmi_smt_header *hdr = (void *)chan->shm_buf.buf;
+	int rc;
+
+	if (!chan->tee)
+		return -ENODEV;
+
+	if (!(hdr->channel_status & SCMI_SHMEM_CHAN_STAT_CHANNEL_FREE)) {
+		dev_err(dev, "SCMI channel busy\n");
+		return -EBUSY;
+	}
+
+	if ((!msg->in_msg && msg->in_msg_sz) ||
+	    (!msg->out_msg && msg->out_msg_sz))
+		return -EINVAL;
+
+	if (chan->shm_buf.size < (sizeof(*hdr) + msg->in_msg_sz) ||
+	    chan->shm_buf.size < (sizeof(*hdr) + msg->out_msg_sz)) {
+		dev_err(dev, "buffer too small\n");
+		return -ETOOSMALL;
+	}
+
+	hdr->channel_status &= ~SCMI_SHMEM_CHAN_STAT_CHANNEL_FREE;
+	hdr->length = msg->in_msg_sz + sizeof(hdr->msg_header);
+	hdr->msg_header = SMT_HEADER_TOKEN(0) |
+			  SMT_HEADER_MESSAGE_TYPE(0) |
+			  SMT_HEADER_PROTOCOL_ID(msg->protocol_id) |
+			  SMT_HEADER_MESSAGE_ID(msg->message_id);
+
+	memcpy(hdr->msg_payload, msg->in_msg, msg->in_msg_sz);
+
+	memset(&arg, 0, sizeof(arg));
+	arg.func = TA_CMD_PROCESS_CHANNEL;
+	arg.session = chan->tee_session;
+
+	memset(param, 0, sizeof(param));
+	param[0].attr = TEE_PARAM_ATTR_TYPE_VALUE_INPUT;
+	param[0].u.value.a = chan->channel_id;
+
+	rc = tee_invoke_func(chan->tee, &arg, ARRAY_SIZE(param), param);
+
+	if (rc) {
+		dev_err(chan->dev, "Failed to invoke OP-TEE\n");
+	} else if (hdr->channel_status & SCMI_SHMEM_CHAN_STAT_CHANNEL_ERROR) {
+		dev_err(chan->dev, "Failed to invoke OP-TEE\n");
+		rc = -EIO;
+	} else {
+		rc = tee_ret2errno(arg.ret);
+	}
+
+	hdr->channel_status |= SCMI_SHMEM_CHAN_STAT_CHANNEL_FREE;
+
+	if (!rc) {
+		if (hdr->length > msg->out_msg_sz + sizeof(hdr->msg_header))
+			return -ETOOSMALL;
+
+		msg->out_msg_sz = hdr->length - sizeof(hdr->msg_header);
+		memcpy(msg->out_msg, hdr->msg_payload, msg->out_msg_sz);
+	}
+
+	return rc;
+}
+
+static int optee_remove_agent(struct udevice *dev)
+{
+	struct scmi_agent *agent = dev_get_priv(dev);
+	struct scmi_optee_channel *chan = agent->method_priv;
+	int rc = 0;
+
+	if (chan && chan->tee) {
+		rc = tee_close_session(chan->tee, chan->tee_session);
+		if (!rc)
+			chan->tee = NULL;
+	}
+
+	return rc;
+}
+
+struct method_ops optee_channel_ops = {
+	.process_msg = optee_process_msg,
+	.remove_agent = optee_remove_agent,
+};
+
+static int probe_optee_channel(struct udevice *dev)
+{
+	struct scmi_agent *agent = dev_get_priv(dev);
+	u32 channel_id;
+	struct scmi_optee_channel *chan;
+	int rc;
+
+	chan = devm_kzalloc(dev, sizeof(*chan), GFP_KERNEL);
+	if (!chan)
+		return -ENOMEM;
+
+	if (!dev_read_u32(dev, "optee-channel-id", &channel_id))
+		dev_info(dev, "No channel ID specified, assume 0\n");
+	else
+		chan->channel_id = channel_id;
+
+	rc = get_shm_buffer(dev, &chan->shm_buf);
+	if (rc) {
+		dev_err(dev, "Failed to get shm resources: %d\n", rc);
+		goto out;
+	}
+
+	rc = open_optee_session(chan);
+	if (!rc)
+		rc = get_optee_channel(chan);
+
+out:
+	if (rc) {
+		devm_kfree(dev, chan);
+	} else {
+		agent->method_ops = &optee_channel_ops;
+		agent->method_priv = (void *)chan;
+	}
+
+	return rc;
+}
+
 /*
  * Exported functions by the SCMI agent
  */
@@ -365,6 +591,7 @@ static int scmi_remove(struct udevice *dev)
 enum scmi_transport_channel {
 	SCMI_MAILBOX_TRANSPORT,
 	SCMI_ARM_SMCCC_TRANSPORT,
+	SCMI_OPTEE_TRANSPORT,
 };
 
 static int scmi_probe(struct udevice *dev)
@@ -377,6 +604,10 @@ static int scmi_probe(struct udevice *dev)
 	case SCMI_ARM_SMCCC_TRANSPORT:
 		if (IS_ENABLED(CONFIG_ARM_SMCCC))
 			return probe_arm_smc_channel(dev);
+		break;
+	case SCMI_OPTEE_TRANSPORT:
+		if (IS_ENABLED(CONFIG_OPTEE))
+			return probe_optee_channel(dev);
 		break;
 	default:
 		break;
@@ -431,6 +662,9 @@ static const struct udevice_id scmi_ids[] = {
 #endif
 #ifdef CONFIG_ARM_SMCCC
 	{ .compatible = "arm,scmi-smc", .data = SCMI_ARM_SMCCC_TRANSPORT },
+#endif
+#ifdef CONFIG_OPTEE
+	{ .compatible = "linaro,scmi-optee", .data = SCMI_OPTEE_TRANSPORT },
 #endif
 	{ }
 };
